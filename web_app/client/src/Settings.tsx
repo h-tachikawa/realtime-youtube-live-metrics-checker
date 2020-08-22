458276a0b8f92f951896c170a55d613a6f9516e3
tachikawa <tachikawa@feedforce.jp>
2020-08-22 15:21:20 +0900

    Adjust styles in settings page

--- web_app/client/src/Settings.tsx
+++ web_app/client/src/Settings.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Container, Divider, Header, Form, Button } from "semantic-ui-react";
+import { Container, Divider, Header, Form, Button, Popup, Icon, Grid } from "semantic-ui-react";
 import { useHistory } from "react-router-dom";
 import { firestore } from "./external/firebase";
 import { AppHeader } from "./AppHeader";
 import { LiveSetting } from "./type";
 import { useGlobalState } from "./state";
-import style from "./App.module.scss";
+import style from "./Settings.module.scss";
 
 const SettingsHeader: React.FC = () => (
     <div>
@@ -49,10 +49,29 @@ const Settings: React.FC = () => {
       <SettingsHeader />
       <Form>
         <Form.Field inline>
-          <label>配信ID</label>
-          <input defaultValue={liveId} onChange={(e) => setLiveId(e.target.value)} />
+          <Grid columns={2} verticalAlign="middle">
+            <Grid.Row>
+              <Grid.Column width="2">
+                <label className={style.helpTipLabel}>配信ID</label>
+                <Popup
+                    trigger={<Icon name="help circle" color="grey" />}
+                    content="Youtube Live配信URLの、v=の後の文字列を入力してください。"
+                    size="mini"
+                    inverted
+                    className={style.popup}
+                />
+              </Grid.Column>
+              <Grid.Column width="3">
+                <input defaultValue={liveId} onChange={(e) => setLiveId(e.target.value)} />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Button type="submit" primary onClick={handleSubmit}>保存</Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Form.Field>
-        <Button type="submit" primary onClick={handleSubmit}>保存</Button>
       </Form>
     </Container>
   );
