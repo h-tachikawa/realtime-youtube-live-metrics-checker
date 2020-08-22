058e4e0abce067634c44b8abc158ae203ffa1058
tachikawa <tachikawa@feedforce.jp>
2020-08-22 14:37:07 +0900

    Extract firebase settings to env file

--- web_app/client/src/external/firebase.ts
+++ web_app/client/src/external/firebase.ts
@@ -1,14 +1,16 @@
 import firebase from "firebase";
 
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+const firebaseConfig = {
+  apiKey:  process.env.REACT_APP_API_KEY,
+  authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL:  process.env.REACT_APP_DATABASE_URL,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:  process.env.REACT_APP_APP_ID,
 };
+
+console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
