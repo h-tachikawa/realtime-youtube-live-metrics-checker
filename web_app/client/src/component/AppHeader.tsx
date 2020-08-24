2455cb95e60a40fca7df6d64a3131e408f6a7ec4
tachikawa <tachikawa@feedforce.jp>
2020-08-24 19:36:13 +0900

    Improve interface of AppHeader component

--- web_app/client/src/component/AppHeader.tsx
+++ web_app/client/src/component/AppHeader.tsx
@@ -1,26 +1,35 @@
 import React from "react";
 import { Icon, Menu, Popup } from "semantic-ui-react";
-import { History } from "history";
 import style from "./App.module.scss";
 import myStyle from "./AppHeader.module.scss";
 
 interface Props {
-  history: History;
-  pathname: string;
+  dashboard: {
+    onClick: () => void;
+  };
+  settings: {
+    onClick: () => void;
+  }
+  currentPath: string;
 }
 
-export const AppHeader: React.FC<Props> = ({history, pathname}) => {
+export const AppHeader: React.FC<Props> = ({dashboard, settings, currentPath}) => {
   return (
       <header className={style.menuContainer}>
         <Menu fixed="top" pointing secondary size="large" className={style.menuContainer} inverted>
-          <Menu.Item link header onClick={() => history.push("/dashboard")}>
+          <Menu.Item link header onClick={dashboard.onClick}>
             Realtime Youtube Live Metrics Checker
           </Menu.Item>
-          <Menu.Item link active={pathname === "/dashboard"} className={style.menuItem} onClick={() => history.push("/dashboard")}>
+          <Menu.Item
+              link
+              active={currentPath === "/dashboard" || currentPath === "/"}
+              className={style.menuItem}
+              onClick={dashboard.onClick}
+          >
             <Icon name="dashboard"/>
             ダッシュボード
           </Menu.Item>
-          <Menu.Item link active={pathname === "/settings"} className={style.menuItem} onClick={() => history.push("/settings")}>
+          <Menu.Item link active={currentPath === "/settings"} className={style.menuItem} onClick={settings.onClick}>
             <Icon name="setting"/>
             設定
           </Menu.Item>
